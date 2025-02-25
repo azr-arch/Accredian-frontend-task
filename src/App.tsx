43e1f36c273ad8f29c3ea6949a9bbc966c00b6f7
Azar <79351619+azr-arch@users.noreply.github.com>
2025-02-25 23:16:18 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { AnnouncementBar } from "./components/announcement-bar";
+
+function App() {
+    return (
+        <div>
+            <AnnouncementBar />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/announcement-bar.tsx
@@ -0,0 +1,8 @@
+export const AnnouncementBar = () => {
+    return (
+        <div className="w-full py-4 flex items-center justify-center">
+            <p className="mr-3">Navigate your ideal career path with tailored expert advice</p>
+            <span>Contact Expert</span>
+        </div>
+    );
+};
